1cd90fcd16b6729249d1eb348500ad77f8994867
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-06-28 16:25:47 +0300

    fix gitigonre file

--- index.js
+++ index.js
@@ -1,80 +1,70 @@
-const theForm = document.querySelector(".input-form");
+const theForm = document.querySelector('.input-form');
 const titleField = theForm.title;
 const authorField = theForm.author;
-const booksContiner = document.querySelector(".books-section")
+const booksContiner = document.querySelector('.books-section');
 
 // get the list of box from the localStorage
 const newbook = JSON.parse(localStorage.getItem('books')) || [];
 
+// ading new book to the list function
+function addNewBook(title, author) {
+  newbook.push(
+    {
+      title,
+      author,
+    },
+  );
 
-//ading new book to the list function
-function addNewBook(title, author){
-    newbook.push(
-       {title,
-        author,}
-    );
+  localStorage.setItem('books', JSON.stringify(newbook));
 
-    localStorage.setItem('books', JSON.stringify(newbook));
-
-    return{title, author}
-
+  return { title, author };
 }
 
-function createBook({title , author}, index){
-    const divforbook = document.createElement('div');
-    const bookName = document.createElement('h2');
-    const bookAuthor = document.createElement('h2');
-    const removeButton = document.createElement('button');
-    const hr = document.createElement('hr');
+function createBook({ title, author }, index) {
+  const divforbook = document.createElement('div');
+  const bookName = document.createElement('h2');
+  const bookAuthor = document.createElement('h2');
+  const removeButton = document.createElement('button');
+  const hr = document.createElement('hr');
 
-    divforbook.classList.add("book-div")
-    // elements contects
-    removeButton.innerHTML = 'Remove';
-    bookName.innerHTML = title;
-    bookAuthor.innerHTML = author;
+  divforbook.classList.add('book-div');
+  // elements contects
+  removeButton.innerHTML = 'Remove';
+  bookName.innerHTML = title;
+  bookAuthor.innerHTML = author;
 
+  removeButton.addEventListener('click', (event) => {
+    event.target.parentElement.remove();
+    newbook.splice(index, 1);
+    localStorage.setItem('books', JSON.stringify(newbook));
+  });
 
-    removeButton.addEventListener('click', (event) =>{
-        event.target.parentElement.remove();
-        newbook.splice(index, 1);
-        localStorage.setItem('books', JSON.stringify(newbook));
-    });
+  divforbook.append(bookName, bookAuthor, removeButton, hr);
 
-    divforbook.append(bookName, bookAuthor, removeButton);
-
-    booksContiner.appendChild(divforbook);
-};
-
-
-const buttonAdd = document.querySelector(".add-button");
+  booksContiner.appendChild(divforbook);
+}
 
 newbook.forEach(createBook);
 
+theForm.onsubmit = (event) => {
+  event.preventDefault();
 
-theForm.onsubmit = (event) =>{
-    event.preventDefault();
+  const thenewbook = addNewBook(titleField.value, authorField.value);
 
-    const thenewbook = addNewBook(titleField.value, authorField.value);
+  createBook(thenewbook);
+};
 
-    createBook(thenewbook);
-}
+// another way of adding books
 
-
-
-
-
-
+// const buttonAdd = document.querySelector('.add-button');
 
 // buttonAdd.addEventListener('click', (e)=>{
 //     e.preventDefault();
 
 //     addNewBook(titleField.value,authorField.value);
-    
+
 //     createBook(addNewBook(titleField.value,authorField.value));
 
 //     titleField.value ='';
 //     authorField.value = '';
 // });
-
-
-    
