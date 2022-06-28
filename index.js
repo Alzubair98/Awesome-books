4debe6d962eebcf3b1b4e7f2746c5b9bfbfe7c07
lucascr <lcrodrigues1995@gmail.com>
2022-06-28 14:26:41 -0300

    change syntax to use js classes

--- index.js
+++ index.js
@@ -2,61 +2,94 @@ const theForm = document.querySelector('.input-form');
 const titleField = theForm.title;
 const authorField = theForm.author;
 const booksContiner = document.querySelector('.books-section');
+const buttonAdd = document.querySelector('.add-button');
 
 // get the list of box from the localStorage
 const newbook = JSON.parse(localStorage.getItem('books')) || [];
 
 // ading new book to the list function
-function addNewBook(title, author) {
-  newbook.push(
-    {
-      title,
-      author,
-    },
-  );
 
-  localStorage.setItem('books', JSON.stringify(newbook));
+class Books {
+    static addNewBook = (title, author) => {
+    newbook.push(
+      {
+        title,
+        author,
+      },
+    );
 
-  return { title, author };
-}
-
-function createBook({ title, author }, index) {
-  const divforbook = document.createElement('div');
-  const bookName = document.createElement('h2');
-  const bookAuthor = document.createElement('h2');
-  const removeButton = document.createElement('button');
- 
-  //adding classes to items 
-  bookName.classList.add("title-author");
-  bookAuthor.classList.add("title-author");
-  removeButton.classList.add("remove-btn")
-
-  divforbook.classList.add('book-div');
-  // elements contects
-  removeButton.innerHTML = 'Remove';
-  bookName.innerHTML = title;
-  bookAuthor.innerHTML = author;
-
-  removeButton.addEventListener('click', (event) => {
-    event.target.parentElement.remove();
-    newbook.splice(index, 1);
     localStorage.setItem('books', JSON.stringify(newbook));
-  });
 
-  divforbook.append(bookName, bookAuthor, removeButton);
+    return { title, author };
+  }
 
-  booksContiner.appendChild(divforbook);
-}
+  static createBook = ({ title, author }, index) => {
+    const divforbook = document.createElement('div');
+    const bookName = document.createElement('h2');
+    const bookAuthor = document.createElement('h2');
+    const removeButton = document.createElement('button');
+  
+    //adding classes to items 
+    bookName.classList.add("title-author");
+    bookAuthor.classList.add("title-author");
+    removeButton.classList.add("remove-btn")
 
-newbook.forEach(createBook);
+    divforbook.classList.add('book-div');
+    // elements contects
+    removeButton.innerHTML = 'Remove';
+    bookName.innerHTML = title;
+    bookAuthor.innerHTML = author;
 
-theForm.onsubmit = (event) => {
-  event.preventDefault();
+    removeButton.addEventListener('click', (event) => {
+      event.target.parentElement.remove();
+      newbook.splice(index, 1);
+      localStorage.setItem('books', JSON.stringify(newbook));
+    });
 
-  const thenewbook = addNewBook(titleField.value, authorField.value);
+    divforbook.append(bookName, bookAuthor, removeButton);
 
-  createBook(thenewbook);
+    booksContiner.appendChild(divforbook);
+  }
+
+  static createHTML = () => {
+    newbook.forEach(createBook);
+  }
+  
+  static addEventListeners = () => {
+      buttonAdd.addEventListener('click', (e)=>{
+        e.preventDefault();
+    
+        const thenewbook = this.addNewBook(titleField.value,authorField.value);
+    
+        this.createBook(thenewbook);
+    
+        titleField.value ='';
+        authorField.value = '';
+    });
+  }
+  
+  
+
+
+
+
+
+
+
+
+
+
+  // static theForm.onsubmit = (event) => {
+  //   event.preventDefault();
+
+  //   const thenewbook = addNewBook(titleField.value, authorField.value);
+
+  //   createBook(thenewbook);
 };
+
+Books.addEventListeners();
+
+
 
 // another way of adding books
 
@@ -65,9 +98,9 @@ theForm.onsubmit = (event) => {
 // buttonAdd.addEventListener('click', (e)=>{
 //     e.preventDefault();
 
-//     addNewBook(titleField.value,authorField.value);
+//     const thenewbook = addNewBook(titleField.value,authorField.value);
 
-//     createBook(addNewBook(titleField.value,authorField.value));
+//     createBook(thenewbook);
 
 //     titleField.value ='';
 //     authorField.value = '';
